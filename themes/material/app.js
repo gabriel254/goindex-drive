72e48b947a7dddaeb035fdec8836362adcdb95ee
Robert Hunter <12846878+gabriel254@users.noreply.github.com>
2020-05-17 23:11:21 -0400

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -1,6 +1,7 @@
 // load in head necessary static
 document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdui/0.4.3/css/mdui.min.css">');
 document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.8.1/video-js.min.css">');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.8.1/video.min.js"></script>');
 // markdown support
 document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/10.0.0/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
@@ -279,7 +280,6 @@ function file_video(path){
         </a>
       </p>
     </video>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.8.1/video.min.js"></script>
     <script>
     var player = videojs(document.getElementById('myVideo'), {
       controls: true,
@@ -315,8 +315,6 @@ function file_video(path){
     }, function (){
     });
     </script>
-
-
     <p>由于浏览器本身的限制，有些音频/视频格式无法在浏览器中播放，也无法外挂字幕文件，但可以用下面的播放器直接播放。</p>
     <p>如果你没有对应播放器，可以点击播放按钮下方的"Don't have IINA"或"Don't have nPlaer"链接进行下载。</p>
     <br>${playBtn}
