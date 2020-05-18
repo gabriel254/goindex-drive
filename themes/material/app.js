6059caaa33d7aeef72b5a693d2653cf7dd3998f0
Robert Hunter <12846878+gabriel254@users.noreply.github.com>
2020-05-17 21:17:08 -0400

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -270,11 +270,55 @@ function file_video(path){
 	var content = `
 <div class="mdui-container-fluid">
 	<br>
-  	<video id="my-video" class="video-js" controls preload="auto" width="640" height="264" data-setup="{}">
-    	<source src="${url}" type="video/mp4" />
+  	<video-js id="myVideo" class="video-js" controls preload="auto" width="640" height="264" data-setup="{}">
     <p class="vjs-no-js"> To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-  </video>
+  </video-js>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.8.1/video.min.js"></script>
+  <script>
+var player = videojs(document.getElementById('myVideo'), {
+  controls: true,
+  preload: 'auto',
+  autoplay: false,
+  fluid: true, // 自适应宽高
+  language: 'zh-CN', // 设置语言
+  muted: false, // 是否静音
+  inactivityTimeout: false,
+  controlBar: { // 设置控制条组件
+    /* 设置控制条里面组件的相关属性及显示与否
+    'currentTimeDisplay':true,
+    'timeDivider':true,
+    'durationDisplay':true,
+    'remainingTimeDisplay':false,
+    volumePanel: {
+      inline: false,
+    }
+    */
+    /* 使用children的形式可以控制每一个控件的位置，以及显示与否 */
+    children: [
+      {name: 'playToggle'}, // 播放按钮
+      {name: 'currentTimeDisplay'}, // 当前已播放时间
+      {name: 'progressControl'}, // 播放进度条
+      {name: 'durationDisplay'}, // 总时间
+      { // 倍数播放
+        name: 'playbackRateMenuButton',
+        'playbackRates': [0.5, 1, 1.5, 2, 2.5]
+      },
+      {
+        name: 'volumePanel', // 音量控制
+        inline: false, // 不使用水平方式
+      },
+      {name: 'FullscreenToggle'} // 全屏
+    ]
+  },
+  sources:[ // 视频源
+      {
+          src: '${url}',
+          type: 'video/mp4'
+      }
+  ]
+}, function (){
+});
+</script>
 	<p>由于浏览器本身的限制，有些音频/视频格式无法在浏览器中播放，也无法外挂字幕文件，但可以用下面的播放器直接播放。</p>
 	<p>如果你没有对应播放器，可以点击播放按钮下方的"Don't have IINA"或"Don't have nPlaer"链接进行下载。</p>
 	<br>${playBtn}
